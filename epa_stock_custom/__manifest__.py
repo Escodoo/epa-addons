6333724807d47fd0048b6e439fe7eb65c3787ed9
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-25 06:49:21 -0300

    [ADD] epa_stock_custom: add new module

---
+++ epa_stock_custom/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Epa Stock Custom",
+    "summary": """
+        EPA Stock Custom""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/epa-addons",
+    "depends": ["stock_request"],
+    "data": ["views/report_epa_stockpicking_operations.xml"],
+}
