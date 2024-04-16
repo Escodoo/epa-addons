61546eb105328238d34d368e570f7f8179291c35
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-04-16 17:45:23 -0300

    [ADD] epa_hr_expense_advance_clearing_custom: add new module

---
+++ epa_hr_expense_advance_clearing_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Epa Hr Expense Advance Clearing Custom",
+    "summary": """
+        EPA HR Expense Advance Clearing Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/epa-addons",
+    "depends": ["hr_expense_advance_clearing"],
+    "data": [
+        "views/hr_expense_sheet.xml",
+    ],
+    "demo": [],
+}
