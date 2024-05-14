59801e26e4f010b02dedd7bfd08e26708cf190b2
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-05-14 15:14:34 -0300

    [ADD] crm_partial: add new module

---
+++ epa_crm_partial/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Crm Partial",
+    "summary": """
+        CRM Partial""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/epa-addons",
+    "depends": ["crm"],
+    "data": [
+        "views/crm_lead.xml",
+    ],
+}
